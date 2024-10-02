ca806e5a4bd54002ab759e062a30f0ae9e2dd498
Frederick Roth <f-roth@megaera.de>
2024-10-02 09:30:02 +0200

    Change Op encoding from c style enum to enum with values.
    This increases bytecodesize by at least factor 2 but makes everything
    much easier. Unsure whether this makes a performance difference on 64
    bit machines anyways.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use chunk::Chunk;
 use miette::{NamedSource, SourceSpan};
+use op::Op;
 use tracing::{debug, Level};
 use tracing_subscriber::FmtSubscriber;
 
 mod chunk;
 mod lox_vector;
 mod memory;
+mod op;
 mod value;
 fn main() {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
         .finish();
 
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing::subscriber::set_global_default(subscriber).unwrap();
 
     let src = "return 1.1;
 return 1.1;
@@ -25,18 +27,17 @@ return;
     let _ = chunk.add_constant(0.0);
     let _ = chunk.add_constant(0.0);
     let _ = chunk.add_constant(0.0);
-    chunk.write_op_code(chunk::OpCode::Return, SourceSpan::from((0, 6)));
+    chunk.write(Op::Return, SourceSpan::from((0, 6)));
     let constant = chunk.add_constant(1.1);
-    chunk.write_op_code(chunk::OpCode::Constant, SourceSpan::from((7, 3)));
-    chunk.write(constant, SourceSpan::from((7, 3)));
-    chunk.write_op_code(chunk::OpCode::Return, SourceSpan::from((12, 6)));
+    chunk.write(Op::Constant(constant), SourceSpan::from((7, 3)));
+    chunk.write(Op::Return, SourceSpan::from((12, 6)));
     let constant = chunk.add_constant(1.1);
-    chunk.write_op_code(chunk::OpCode::Constant, SourceSpan::from((19, 3)));
-    chunk.write(constant, SourceSpan::from((19, 3)));
-    chunk.write_op_code(chunk::OpCode::Return, SourceSpan::from((24, 6)));
+    chunk.write(Op::Constant(constant), SourceSpan::from((19, 3)));
+    chunk.write(Op::Return, SourceSpan::from((24, 6)));
     chunk.disassemble(&src);
 
     debug!("{}", chunk.disassemble_at(&src, 0));
     debug!("{}", chunk.disassemble_at(&src, 1));
-    debug!("{}", chunk.disassemble_at(&src, 3));
+    debug!("{}", chunk.disassemble_at(&src, 2));
+    debug!("{}", chunk.disassemble_at(&src, 4));
 }
