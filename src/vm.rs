30ad1579e276fbb15ada06cf26035ad999c24dbb
Frederick Roth <f-roth@megaera.de>
2025-01-11 22:09:42 +0100

    Move chunk to vm struct

--- src/vm.rs
+++ src/vm.rs
@@ -1,6 +1,6 @@
-use std::{fmt::Write as _, ops::Deref};
+use std::{fmt::Write as _, ops::Deref, ptr::null};
 
-use miette::{LabeledSpan, NamedSource};
+use miette::{LabeledSpan, NamedSource, SourceSpan};
 use tracing::debug;
 
 use crate::{
@@ -22,10 +22,12 @@ pub struct VM {
     gc: Gc,
     globals: HashTable,
     printer: Box<dyn Printer>,
+    current: Chunk,
+    ip: *const Op,
 }
 
 macro_rules! binary_operator {
-    ($self: ident, $chunk: ident, $i: ident, $op:tt, $constructor: expr) => {
+    ($self: ident, $op:tt, $constructor: expr) => {
         {
             if let (Value::Number(a), Value::Number(b)) = ($self.peek(1), $self.peek(0)) {
                 let _ = $self.pop();
@@ -33,7 +35,7 @@ macro_rules! binary_operator {
                 $self.push($constructor(a $op b));
             } else {
                 miette::bail!(
-                    labels = vec![LabeledSpan::at($chunk.locations[$i], "here")],
+                    labels = vec![LabeledSpan::at($self.current_location(), "here")],
                     "Operands for operation must be both be numbers"
                 );
             }
@@ -47,12 +49,16 @@ impl VM {
         let stack_top = stack.as_mut_ptr();
         let gc = Gc::new();
         let globals = HashTable::new();
+        let chunk = Chunk::new();
+        let ip: *const Op = null();
         Self {
             stack,
             stack_top,
             gc,
             globals,
             printer: Box::new(ConsolePrinter),
+            current: chunk,
+            ip,
         }
     }
 
@@ -65,7 +71,9 @@ impl VM {
             Err(e) => return Err(InterpreterError::CompileError(e.with_source_code(src))),
         };
 
-        match self.interpret_inner(&src, chunk) {
+        self.set_chunk(chunk);
+
+        match self.interpret_inner(&src) {
             Ok(value) => Ok(value),
             Err(e) => {
                 self.reset_stack();
@@ -74,23 +82,31 @@ impl VM {
         }
     }
 
-    fn interpret_inner(&mut self, src: &NamedSource<String>, chunk: Chunk) -> miette::Result<()> {
-        let start: *const Op = &chunk.code[0];
-        let mut ip: *const Op = start;
+    fn set_chunk(&mut self, chunk: Chunk) {
+        self.ip = &chunk.code[0];
+        self.current = chunk;
+    }
+
+    fn current_index(&self) -> usize {
+        unsafe { self.ip.offset_from(&self.current.code[0]) as usize }
+    }
+
+    fn current_location(&self) -> SourceSpan {
+        self.current.locations[self.current_index()]
+    }
+
+    fn interpret_inner(&mut self, src: &NamedSource<String>) -> miette::Result<()> {
         loop {
-            let op = unsafe { *ip };
-            //TODO: i has only to be computed in error cases.
-            // In clox this is achieved by ip being a global variable. Performance should be better if ip would be part of vm?
-            let i = unsafe { ip.offset_from(start) as usize };
+            let op = unsafe { *self.ip };
             unsafe {
-                ip = ip.add(1);
+                self.ip = self.ip.add(1);
             }
-            debug!("{}", chunk.disassemble_at(src, i));
+            debug!("{}", self.current.disassemble_at(src, self.current_index()));
             debug!("          {}", self.trace_stack());
             match op {
                 Op::Return => return Ok(()),
                 Op::Constant(index) => {
-                    let constant = chunk.constants[index as usize];
+                    let constant = self.current.constants[index as usize];
                     self.push(constant);
                 }
                 Op::Nil => self.push(Value::Nil),
@@ -103,15 +119,15 @@ impl VM {
                         self.push(Value::Number(-number));
                     } else {
                         miette::bail!(
-                            labels = vec![LabeledSpan::at(chunk.locations[i], "here")],
+                            labels = vec![LabeledSpan::at(self.current_location(), "here")],
                             "Operand for unary - must be a number"
                         );
                     }
                 }
-                Op::Add => self.plus_operator(&chunk, i)?,
-                Op::Subtract => binary_operator!(self, chunk,i,  -, Value::Number),
-                Op::Multiply => binary_operator!(self, chunk,i, *, Value::Number),
-                Op::Divide => binary_operator!(self, chunk,i, /, Value::Number),
+                Op::Add => self.plus_operator()?,
+                Op::Subtract => binary_operator!(self, -, Value::Number),
+                Op::Multiply => binary_operator!(self, *, Value::Number),
+                Op::Divide => binary_operator!(self, /, Value::Number),
                 Op::Not => {
                     let pop = self.pop();
                     self.push(Value::Boolean(pop.is_falsey()))
@@ -121,8 +137,8 @@ impl VM {
                     let a = self.pop();
                     self.push(Value::Boolean(a == b));
                 }
-                Op::Greater => binary_operator!(self, chunk,i, >, Value::Boolean),
-                Op::Less => binary_operator!(self, chunk,i, <, Value::Boolean),
+                Op::Greater => binary_operator!(self, >, Value::Boolean),
+                Op::Less => binary_operator!(self, <, Value::Boolean),
                 Op::Print => {
                     let res = self.pop();
                     self.printer.print(res);
@@ -131,29 +147,29 @@ impl VM {
                     self.pop();
                 }
                 Op::DefineGlobal(index) => {
-                    let name = chunk.constants[index as usize];
+                    let name = self.current.constants[index as usize];
                     self.globals.insert(name, self.peek(0));
                     self.pop();
                 }
                 Op::GetGlobal(index) => {
-                    let name = chunk.constants[index as usize];
+                    let name = self.current.constants[index as usize];
                     if let Some(v) = self.globals.get(name) {
                         self.push(v)
                     } else {
                         miette::bail!(
-                            labels = vec![LabeledSpan::at(chunk.locations[i], "here")],
+                            labels = vec![LabeledSpan::at(self.current_location(), "here")],
                             "Undefined variable {}",
                             name
                         )
                     }
                 }
                 Op::SetGlobal(index) => {
-                    let name = chunk.constants[index as usize];
+                    let name = self.current.constants[index as usize];
                     let inserted = self.globals.insert(name, self.peek(0));
                     if inserted {
                         self.globals.delete(name);
                         miette::bail!(
-                            labels = vec![LabeledSpan::at(chunk.locations[i], "here")],
+                            labels = vec![LabeledSpan::at(self.current_location(), "here")],
                             "Undefined variable {}",
                             name
                         )
@@ -165,14 +181,14 @@ impl VM {
                 Op::SetLocal(slot) => self.stack[slot as usize] = self.peek(0),
                 Op::JumpIfFalse(offset) => {
                     if self.peek(0).is_falsey() {
-                        unsafe { ip = ip.add((offset - 1) as usize) }
+                        unsafe { self.ip = self.ip.add((offset - 1) as usize) }
                     }
                 }
             }
         }
     }
 
-    fn plus_operator(&mut self, chunk: &Chunk, index: usize) -> miette::Result<()> {
+    fn plus_operator(&mut self) -> miette::Result<()> {
         match (self.peek(1), self.peek(0)) {
             (Value::Number(a), Value::Number(b)) => {
                 self.pop();
@@ -187,13 +203,13 @@ impl VM {
                     self.push(Value::Obj(concated));
                 } else {
                     miette::bail!(
-                        labels = vec![LabeledSpan::at(chunk.locations[index], "here")],
+                        labels = vec![LabeledSpan::at(self.current_location(), "here")],
                         "Operands for operation must be both be numbers or Strings"
                     )
                 }
             }
             _ => miette::bail!(
-                labels = vec![LabeledSpan::at(chunk.locations[index], "here")],
+                labels = vec![LabeledSpan::at(self.current_location(), "here")],
                 "Operands for operation must be both be numbers or Strings"
             ),
         }
