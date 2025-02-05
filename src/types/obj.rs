0016465e36177ded0bb10a48c6965a08d5dd7358
Frederick Roth <f-roth@megaera.de>
2025-02-05 16:08:52 +0100

    Implement native functions

--- src/types/obj.rs
+++ src/types/obj.rs
@@ -2,11 +2,14 @@ use std::fmt::Display;
 
 use super::function::Function;
 use super::string::LoxString;
+use super::value::Value;
 use super::Hashable;
+use crate::types::Hash;
 
 pub enum Obj {
     String(LoxString),
     Function(Function),
+    Native(fn(u8, *mut Value) -> Value),
 }
 
 impl Hashable for Obj {
@@ -14,6 +17,7 @@ impl Hashable for Obj {
         match self {
             Obj::String(lox_string) => lox_string.hash(),
             Obj::Function(function) => function.hash(),
+            Obj::Native(_) => Hash(11),
         }
     }
 }
@@ -23,6 +27,7 @@ impl Display for Obj {
         match self {
             Obj::String(s) => write!(f, "{}", s.string),
             Obj::Function(function) => write!(f, "{}", function),
+            Obj::Native(_) => write!(f, "<native fn>"),
         }
     }
 }
